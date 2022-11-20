f0cee30bda44462ac9a07aa28107086450958901
pny912 <pnylecture1@gmail.com>
2022-11-20 12:59:54 +0500

    Add New TOast Method

--- app/src/main/java/com/pny/github/MainActivity.java
+++ app/src/main/java/com/pny/github/MainActivity.java
@@ -3,6 +3,7 @@ package com.pny.github;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MyToast("Message");
+    }
+
+    private void MyToast(String message) {
+
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 }
